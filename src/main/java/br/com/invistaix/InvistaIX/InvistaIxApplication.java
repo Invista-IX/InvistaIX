77fbf49d8c3d48b42cfa8a43df75c4c4fa5d0e79
Arthur Haupt Pacheco <arthurhauptpacheco@gmail.com>
2025-04-28 20:38:09 -0300

    chore: setup inicial do projeto com estrutura base do Spring Boot

---
+++ src/main/java/br/com/invistaix/InvistaIX/InvistaIxApplication.java
@@ -0,0 +1,13 @@
+package br.com.invistaix.InvistaIX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvistaIxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvistaIxApplication.class, args);
+	}
+
+}
