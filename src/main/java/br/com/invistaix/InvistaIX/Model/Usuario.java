63d31d8c56aea7cd6400857c59ec86bff6988a0e
Arthur Haupt Pacheco <arthurhauptpacheco@gmail.com>
2025-05-04 15:18:39 -0300

    feat: removido localDate e alterado incrementado as column e geters e seters no Entity Usuario .

--- src/main/java/br/com/invistaix/InvistaIX/Model/Usuario.java
+++ src/main/java/br/com/invistaix/InvistaIX/Model/Usuario.java
@@ -1,25 +1,96 @@
 package br.com.invistaix.InvistaIX.Model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.time.LocalDate;
 
 @Entity
-@Data
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "idgestor")
+    private int id;
 
+    @Column(nullable = false, length = 45)
     private String nome;
+
+    @Column(nullable = false, length = 45)
+    private String sobrenome;
+
+    @Column(length = 100)
     private String email;
-    private String senha;
-    private String cpfCnpj;
+
+    @Column(length = 16)
     private String telefone;
+
+    @Column(name = "CNPJ_CPF", nullable = false, length = 14)
+    private String cpfCnpj;
+
+    @Column(nullable = false, length = 45)
+    private String senha;
+
+    @Column(name = "pessoa_FJ", nullable = false, length = 1)
     private String tipoPessoa;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDate dataNascimento;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipoPessoa() {
+        return tipoPessoa;
+    }
+
+    public void setTipoPessoa(String tipoPessoa) {
+        this.tipoPessoa = tipoPessoa;
+    }
 }
