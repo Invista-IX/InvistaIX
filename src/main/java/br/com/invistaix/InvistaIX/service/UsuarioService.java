2621628aa2218ebc06df9447266e171abe412b43
gustagueimer <gustavomarqueslima@outlook.com>
2025-05-05 19:17:35 -0300

    feat: implementação das funcionalidades do UsuarioService

--- src/main/java/br/com/invistaix/InvistaIX/service/UsuarioService.java
+++ src/main/java/br/com/invistaix/InvistaIX/service/UsuarioService.java
@@ -5,7 +5,44 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class CadastroService {
+import br.com.invistaix.InvistaIX.model.Usuario;
+import br.com.invistaix.InvistaIX.repository.UsuarioRepository;
 
+@Service
+public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	public List<Usuario> listarTodos() {
+		return usuarioRepository.findAll();
+	}
+	
+	public Usuario encontrarPorId(Integer id) {
+		return usuarioRepository.findById(id).orElse(null);
+	}
+	
+	public Usuario encontrarPorEmail(String email) {
+		return usuarioRepository.findByEmail(email);
+	}
+	
+	public Usuario salvarCadastro(Usuario cadastro) {
+		return usuarioRepository.save(cadastro);
+	}
+	
+	public void apagarUsuario(Integer id) {
+		usuarioRepository.deleteById(id);
+	}
+	
+	public Boolean checarCPFCadastrado(Usuario usuario) {
+		return usuarioRepository.existsByCpfCnpj(usuario.getCpfCnpj());
+	}
+	
+	public Boolean checarEmailCadastrado(Usuario usuario) {
+		return usuarioRepository.existsByEmail(usuario.getEmail());
+	}
+	
+	public Boolean checarTelefoneCadastrado(Usuario usuario) {
+		return usuarioRepository.existsByEmail(usuario.getTelefone());
+	}
 }
