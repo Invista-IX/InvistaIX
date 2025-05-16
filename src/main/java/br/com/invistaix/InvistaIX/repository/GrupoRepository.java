14272000e768640aec0adde33699a79bd85e8dc1
gustagueimer <gustavomarqueslima@outlook.com>
2025-05-16 20:27:04 -0300

    feat: implementação completa do GrupoRepository.java

--- src/main/java/br/com/invistaix/InvistaIX/repository/GrupoRepository.java
+++ src/main/java/br/com/invistaix/InvistaIX/repository/GrupoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.invistaix.InvistaIX.model.Grupo;
 
-public interface GrupoRepository extends JpaRepository {
-
+public interface GrupoRepository extends JpaRepository<Grupo, Integer> {
+	boolean existsByCodigo(String codigo);
+	Grupo findByCodigo(String codigo);
 }
