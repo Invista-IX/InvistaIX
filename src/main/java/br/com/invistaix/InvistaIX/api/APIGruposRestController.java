a095dbe68b621d3093843490286a8fee22fe0577
gustagueimer <gustavomarqueslima@outlook.com>
2025-05-16 21:37:09 -0300

    feat: Criação do diretório /api e implementação do APIGruposRestController.java

---
+++ src/main/java/br/com/invistaix/InvistaIX/api/APIGruposRestController.java
@@ -0,0 +1,24 @@
+package br.com.invistaix.InvistaIX.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.invistaix.InvistaIX.model.Grupo;
+import br.com.invistaix.InvistaIX.service.GrupoService;
+
+@RestController
+@RequestMapping("/api")
+public class APIGruposRestController {
+	
+	@Autowired
+	GrupoService grupoService;
+	
+	@GetMapping("/grupos")
+	public List<Grupo> getGrupos() {
+		return grupoService.listarTodos();
+	}
+}
