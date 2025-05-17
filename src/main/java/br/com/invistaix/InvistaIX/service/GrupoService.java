f2ff637a525da11e3002808397db78c729c4cd55
gustagueimer <gustavomarqueslima@outlook.com>
2025-05-16 21:22:41 -0300

    feat: implementação completa do GrupoService.java

--- src/main/java/br/com/invistaix/InvistaIX/service/GrupoService.java
+++ src/main/java/br/com/invistaix/InvistaIX/service/GrupoService.java
@@ -4,13 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import br.com.invistaix.InvistaIX.model.Grupo;
 import br.com.invistaix.InvistaIX.repository.GrupoRepository;
-import br.com.invistaix.InvistaIX.model.Usuario;
-import br.com.invistaix.InvistaIX.repository.UsuarioRepository;
 
 @Service
 public class GrupoService {
+	
     @Autowired
-
+    GrupoRepository grupoRepository;
+    
+    public void salvar(Grupo novoGrupo) {
+    	grupoRepository.save(novoGrupo);
+    }
+    
+    public List<Grupo> listarTodos() {
+    	return grupoRepository.findAll();
+    }
+    
+    public Grupo encontrarPorId(Integer id) {
+    	return grupoRepository.findById(id).orElse(null);
+    }
+    
+    public boolean conferirExistencia(Grupo grupo) {
+    	return grupoRepository.existsById(grupo.getId());
+    }
 }
