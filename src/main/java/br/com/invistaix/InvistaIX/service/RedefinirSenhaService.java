89d191987097af92cae39135d0013158a1abe685
Alessandra <alessandravitoria799@gmail.com>
2025-05-23 14:24:22 -0300

    fix: Pequenas correções em CadastroController e RedefinirSenhaService

--- src/main/java/br/com/invistaix/InvistaIX/service/RedefinirSenhaService.java
+++ src/main/java/br/com/invistaix/InvistaIX/service/RedefinirSenhaService.java
@@ -1,6 +1,6 @@
 package br.com.invistaix.InvistaIX.service;
 
-import br.com.invistaix.InvistaIX.model.Usuario;
+import br.com.invistaix.InvistaIX.model.UsuarioModel;
 import br.com.invistaix.InvistaIX.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +17,7 @@ public class RedefinirSenhaService {
     private JavaMailSender emailSender;
 
     public boolean enviarLinkRedefinicao(String email) {
-        Usuario usuario = usuarioRepository.findByEmail(email);
+        UsuarioModel usuario = usuarioRepository.findByEmail(email);
         if (usuario != null) {
             String linkRedefinicao = "http://localhost:8080/redefinirsenha?email=" + email;
             enviarEmail(email, linkRedefinicao);
@@ -37,7 +37,7 @@ public class RedefinirSenhaService {
 
 
     public boolean redefinirSenha(String email, String novaSenha) {
-        Usuario usuario = usuarioRepository.findByEmail(email);
+        UsuarioModel usuario = usuarioRepository.findByEmail(email);
         if (usuario != null) {
             usuario.setSenha(novaSenha);
             usuarioRepository.save(usuario);
