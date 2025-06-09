57873a8bf4d9b36c3644b3ff8df63c60d467eff3
arthurfelipe3252 <arthurfelipebalmantdesouza2019@gmail.com>
2025-06-09 19:05:23 -0300

    feat implementa v2 do migration

---
+++ src/main/resources/db/migration/V2__alter_proprietario_endereco.sql
@@ -0,0 +1,14 @@
+alter table endereco
+    add estado varchar(2);
+
+alter table endereco
+    add CEP varchar(8);
+
+alter table imovel
+    add numero_matricula varchar(15);
+
+ALTER TABLE endereco
+    RENAME COLUMN municipio TO cidade;
+
+ALTER TABLE imovel
+    RENAME COLUMN valormatricula TO valor_matricula;
