6d746cf671cbe97642527a135521e1529dae55f1
gustagueimer <gustavomarqueslima@outlook.com>
2025-06-19 21:49:23 -0300

    feat: implementado botões para gerenciar ou analisar imóveis nos cartões de imóvel

--- src/main/resources/static/JS/dashboard/grupo.js
+++ src/main/resources/static/JS/dashboard/grupo.js
@@ -55,19 +55,58 @@ function criarElementoCardImovel(item) {
     const nome = document.createElement("h3");    
     nome.classList.add("card-h3");
     nome.innerText = item.nome;
-
+	
+	const matricula_value = document.createElement("p");
+	matricula_value.classList.add("card-p");
+	matricula_value.innerText = item.numeroMatricula;
+	
     const matricula = document.createElement("p");    
     matricula.classList.add("card-p");
-    matricula.innerText = item.numero_matricula;
+	matricula.classList.add("card-matricula");
+    matricula.innerText = "Numero da Matricula";
+	matricula.appendChild(document.createElement("br"));
+	matricula.appendChild(matricula_value);
+	
+	const preco_value = document.createElement("p");
+	preco_value.classList.add("card-p");
+	preco_value.innerText = item.preco;
+	
+	const preco = document.createElement("p");    
+	preco.classList.add("card-p");
+	preco.classList.add("card-preco");
+	preco.innerText = "Valor";
+	preco.appendChild(document.createElement("br"));
+	preco.appendChild(preco_value);
+	
+	const area_value = document.createElement("p");
+	area_value.classList.add("card-p");
+	area_value.innerText = item.area;
 
     const area = document.createElement("p");    
     area.classList.add("card-p");
-    area.innerText = item.area;
+	area.classList.add("card-area");
+    area.innerText = "Área";
+	area.appendChild(document.createElement("br"));
+	area.appendChild(area_value);
+	
+	const botao_gerenciar = document.createElement("button");
+	botao_gerenciar.classList.add("gerenciar_imovel");
+	botao_gerenciar.innerText = "Gerenciar Imóvel"
+	
+	const gerenciar_link = document.createElement("a");
+	gerenciar_link.classList.add("gerenciar_link");
+	gerenciar_link.href = "/imovel/grupo=" + idGrupo + "&imovel=" + item.id + "/gerenciar";
+	gerenciar_link.append(botao_gerenciar);
+	
+	const botao_analisar = document.createElement("button");
+	botao_analisar.classList.add("analisar_imovel");
+	botao_analisar.innerText = "Analisar Imóvel"
 
-    const preco = document.createElement("p");    
-    preco.classList.add("card-p");
-    preco.innerText = item.preco;
-
+	const analisar_link = document.createElement("a");
+	analisar_link.classList.add("analisar_link");
+	analisar_link.href = "/imovel/grupo=" + idGrupo + "&imovel=" + item.id + "/graficos";
+	analisar_link.append(botao_analisar);
+	
     const imovel_card = document.createElement("div");
     imovel_card.classList.add("card");
     imovel_card.appendChild(imagem);
@@ -75,10 +114,8 @@ function criarElementoCardImovel(item) {
     imovel_card.appendChild(matricula);
     imovel_card.appendChild(area);
     imovel_card.appendChild(preco);
+	imovel_card.appendChild(gerenciar_link);
+	imovel_card.appendChild(analisar_link);
 
-    const imovel_link = document.createElement("a");
-    imovel_link.href = "/imovel/grupo=" + idGrupo + "&imovel=" + item.id + "/gerenciar";
-    imovel_link.append(imovel_card);
-
-    return imovel_link;
+    return imovel_card;
 };
